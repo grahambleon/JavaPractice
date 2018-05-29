e94d23b16b3ceb59f7cf58dc1205f5ad9dbf5a46
Graham Gilpatric <grahamwithag@gmail.com>
2018-05-28 22:47:41 -0400

    Word class works runner does not

--- WordCounter/src/wordcounter/Runner.java
+++ WordCounter/src/wordcounter/Runner.java
@@ -1,10 +1,23 @@
 package wordcounter;
 
+import java.util.Scanner;
+
 public class Runner {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
-
+    System.out.println("Enter some words!");
+    
+    Scanner in = new Scanner(System.in);
+    String input = in.next();
+    in.close();
+    String[] sentence = input.split("\\\\s+");
+    
+    System.out.println(sentence[1]);
+    
+//    for (int i=0; i<sentence.length; i++)
+//    {
+//    	Word word = new Word(sentence[i]);
+//    	System.out.println(word.letters);
+//    }
   }
-
 }
--- WordCounter/src/wordcounter/Word.java
+++ WordCounter/src/wordcounter/Word.java
@@ -1,5 +1,30 @@
 package wordcounter;
 
-public class Word {
+public class Word { 
+  public String letters;
 
+  Word(String letters) {
+    this.letters = letters;
+  }
+  
+  public boolean hasMoreThan(int charCount ) {
+    boolean answer = false;
+    if (this.letters.length() > charCount) {
+      answer = true;
+    }
+    return answer;
+  }
+  
+  public boolean containsVowels() {
+	boolean vowels = false;
+    if ((this.letters.contains("a")) ||
+    	  (this.letters.contains("e")) ||
+    	  (this.letters.contains("i")) ||
+    	  (this.letters.contains("o")) ||
+    	  (this.letters.contains("u")) ||
+    	  (this.letters.contains("y"))) {
+      vowels = true;
+    }
+    return vowels;
+  }
 }
